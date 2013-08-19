3fc6bb3c802bce4e75064f33e004b0f6fc746828
Jun He <junnhe@gmail.com>
2013-08-19 10:14:05 -0700

    now able to save config to file for this walkman

--- src/walkman.py
+++ src/walkman.py
@@ -17,48 +17,112 @@ import time
 import shutil
 import os
 
+
+class Config:
+    def __init__(self):
+        self.dic = {}
+
+    def display(self, style="columns", colwidth=15, save2file=""):
+        contents = ""
+        if style == "columns":
+            header = self.dic.keys()
+            header = [ str(x).ljust(colwidth) for x in header]
+            header = " ".join(header) + '\n'
+
+            vals = self.dic.values()
+            vals = [ str(x).ljust(colwidth) for x in vals]
+            vals = " ".join(vals) + '\n'
+
+            contents = header + vals
+        else:
+            tablestr = ""
+            for keyname in self.dic:
+                k = str(keyname).ljust(colwidth)
+                v = str(self.dic[keyname]).ljust(colwidth)
+                entry = " ".join([k, v]) 
+                tablestr += entry + '\n'
+
+            contents = tablestr
+
+        if save2file != "":
+            with open(save2file, 'w') as f:
+                f.write(contents)
+                f.flush()
+        return contents
+
 class Walkman:
     def __init__(self):
-        self.devname = '/dev/sdb'
-        self.partition = '/dev/sdb1'
-        self.diskconf = '../conf/sfdisk.conf'
-        self.mountpoint = '/mnt/scratch/'
-        self.monitor = MWpyFS.Monitor.FSMonitor(self.partition, 
-                                                 self.mountpoint,
-                                                 ld = "./") # logdir
-        self.jobid = strftime("%Y-%m-%d-%H-%M-%S", localtime())
+        self.conf = Config()
 
+        self.conf.dic['devname'] = '/dev/sdb'
 
-        self.workloadpath = "./pyWorkload/workload.buf"
+        self.conf.dic['partition'] = '/dev/sdb1'
+        self.conf.dic['diskconf'] = '../conf/sfdisk.conf'
+        self.conf.dic['mountpoint'] = '/mnt/scratch/'
+        self.conf.dic['jobid'] = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
+        self.conf.dic['workloadpath'] = "./pyWorkload/workload.buf"
+        self.conf.dic['playerpath'] = "../build/src/player"
+        self.conf.dic['mpirunpath'] = "/home/junhe/installs/openmpi-1.4.3/bin/mpirun"
+        self.conf.dic['resultdir'] = "./results/"
+
+        self.conf.dic['np'] = 3 # put it here guide mpirun and wl producer
+        self.conf.dic['ndir_per_pid'] = 7
+        self.conf.dic['startOff'] = 0
+        self.conf.dic['nwrites_per_file'] = 5
+        self.conf.dic['nfile_per_dir'] = 5
+        self.conf.dic['wsize'] = 5
+        self.conf.dic['wstride'] = 5
+
+        self.conf.dic['HEADERMARKER_walkman_config'] = \
+                'DATAMARKER_walkman_config'
+
+       
+        # monitor
+        self.monitor = MWpyFS.Monitor.FSMonitor(self.conf.dic['partition'], 
+                                                 self.conf.dic['mountpoint'],
+                                                 ld = self.conf.dic['resultdir']) # logdir
+        # producer
         self.wl_producer = pyWorkload.producer.Producer()
-        self.playerpath = "../build/src/player"
-        self.mpirunpath = "/home/junhe/installs/openmpi-1.4.3/bin/mpirun"
-        self.np = 3 # put it here guide mpirun and wl producer
-        self.ndir_per_pid = 7
-        
+
+
+    def displayandsaveConfig(self):
+        colwidth = 30
+        conflog = self.conf.dic['resultdir'] + \
+                    "walkmanJOB-"+self.conf.dic['jobid']+".conf.rows"
+        print self.conf.display(style="rows",
+                                colwidth=colwidth,
+                                save2file=conflog)
+
+        conflog = self.conf.dic['resultdir'] + \
+                    "walkmanJOB-"+self.conf.dic['jobid']+".conf.columns"
+        self.conf.display(style="columns",
+                                colwidth=colwidth,
+                                save2file=conflog)
 
     def rebuildFS(self):
-        MWpyFS.FormatFS.buildNewExt4(self.devname,
-                self.mountpoint, self.diskconf, "junhe")
+        MWpyFS.FormatFS.buildNewExt4(self.conf.dic["devname"],
+                self.conf.dic['mountpoint'], self.conf.dic['diskconf'], "junhe")
 
     #def produceWorkload_rmdir(self, rootdir):
-        #self.wl_producer.produce_rmdir(np=self.np,
+        #self.wl_producer.produce_rmdir(np=self.conf.dic['np'],
                                        #ndir_per_pid=self.ndir_per_pid,
                                        #rootdir=self.mountpoint+rootdir,
                                        #tofile=self.workloadpath)
 
     def produceWorkload(self, rootdir):
-        self.wl_producer.produce(np=self.np, 
-                                startOff=0,
-                                nwrites_per_file = 50, 
-                                nfile_per_dir=3, 
-                                ndir_per_pid=self.ndir_per_pid,
-                                wsize=4097, 
-                                wstride=4097, 
-                                rootdir=self.mountpoint+rootdir,
-                                tofile=self.workloadpath)
+        self.wl_producer.produce(np=self.conf.dic['np'], 
+                                startOff=self.conf.dic['startOff'],
+                                nwrites_per_file = self.conf.dic['nwrites_per_file'], 
+                                nfile_per_dir=self.conf.dic['nfile_per_dir'], 
+                                ndir_per_pid=self.conf.dic['ndir_per_pid'],
+                                wsize=self.conf.dic['wsize'], 
+                                wstride=self.conf.dic['wstride'], 
+                                rootdir=self.conf.dic['mountpoint']+rootdir,
+                                tofile=self.conf.dic['workloadpath'])
     def play(self):
-        cmd = [self.mpirunpath, "-np", self.np, self.playerpath, self.workloadpath]
+        cmd = [self.conf.dic['mpirunpath'], "-np", self.conf.dic['np'], 
+                self.conf.dic['playerpath'], self.conf.dic['workloadpath']]
         cmd = [str(x) for x in cmd]
         proc = subprocess.Popen(cmd) 
         proc.wait()
@@ -68,7 +132,9 @@ class Walkman:
         return rootdir
 
     def getLogFilenameBySeasonYear(self, season, year):
-        return "result.log.season-"+str(season)+".year-"+str(year)
+        return "walkmanJOB-"+self.conf.dic['jobid']+\
+                ".result.log.year-"+str(year).zfill(5)+\
+                ".season-"+str(season).zfill(5)
 
     def displayFreeFrag():
         e2ff = self.monitor.e2freefrag()
@@ -82,6 +148,8 @@ def main():
     #walkman.rebuildFS()
     #return
 
+    walkman.displayandsaveConfig()
+    time.sleep(5)
 
     nyears=2
     nseasons_per_year = 3
@@ -100,13 +168,15 @@ def main():
             # now, delete the previous dir if it exists
             pre_s = (s - (nseasons_per_year-1))%nseasons_per_year
             pre_s_rootdir = walkman.getrootdirByIterIndex(pre_s)
-            fullpath = os.path.join(walkman.mountpoint,pre_s_rootdir)
+            fullpath = os.path.join(walkman.conf.dic['mountpoint'],pre_s_rootdir)
             try:
                 print "removing ", fullpath
                 shutil.rmtree(fullpath)
             except:
                 print "failed to rmtree (but should be OK):", fullpath
 
+            print "------ End of this year, sleep 10 sec ----------"
+            time.sleep(10)
             
 
 if __name__ == "__main__":
