7b8d75d65d1bf7769e161ee4b1f4362b260cf98b
Jun He <junnhe@gmail.com>
2013-08-28 10:21:12 -0500

    added many functions in plot.R

--- scripts/plot.R
+++ scripts/plot.R
@@ -203,7 +203,7 @@ files2df <- function(dirpath)
     #print (str(dflist))
     # put walkman config to all other df
 
-    dfvec = c("_extstats", "_extstatssum", "_freefrag_sum",
+    dfvec = c("_extlist", "_extstats", "_extstatssum", "_freefrag_sum",
               "_freefrag_hist", "_freeblocks", "_freeinodes",
               "_walkman_config")
     conf = dflist[['_walkman_config']][,c("hostname", "jobid", "nyears", "nseasons_per_year",
@@ -270,9 +270,10 @@ plot_one_yvar <- function(df, seecol, n_per_jobid=10)
     #df$wstride = factor(df$wstride)
     df$wsize = factor(df$wsize)
     levels(df$wsize) = paste("WriteSize:", levels(df$wsize))
+    df$ypos = df[,seecol]*0.8
     p <- ggplot(df, aes_string(x="factor(wstride)", y=seecol, fill="monitor_time")) +
         geom_bar(stat='identity', position='dodge', drop=F) +
-        #geom_text(aes(label=fs_nmetablocks), position=position_dodge(width=1))+
+        geom_text(aes_string(label=seecol, y="ypos"), position=position_dodge(width=1), angle=90)+
         scale_x_discrete(drop=F) +
         facet_wrap(~wsize) +
         xlab("Write stride")
@@ -309,24 +310,208 @@ plot_freefrag_sum <- function(df, n_per_jobid=10)
     print(p)
 }
 
+# plot the relation between filepath and number of metablocks
+ss_plot_meta_file <- function(df, n_per_jobid)
+{
+    df = ddply(df, .(jobid, filepath), head, n=n_per_jobid)
+    
+
+    df = subset(df, wsize==1 & monitor_time=='year00009.season00003')
+    print (df)
+    df$wstride = factor(df$wstride)
+    df$wsize = factor(df$wsize)
+    # clearer annotation
+    levels(df$wstride) = paste("Stride:", levels(df$wstride))
+    levels(df$wsize) = paste("Size:", levels(df$wsize))
+
+    p <- ggplot(df, aes(x=filepath, y=n_metablock, fill=monitor_time), drop=F) +
+        geom_bar(stat='identity', position='dodge')+
+        facet_grid(wstride~wsize)+
+        theme(axis.text.x=element_text(angle=45,hjust=1))
+    print(p)
+}
+
 ss_main <- function() 
 {
     #list.ss = files2df("C:/Users/Jun/Dropbox/0-Research/0-metadata/datahub/results.h0.wsize.wstride")
-    
-    #df = list.ss[['_extstatssum']]
-    #plot_one_yvar(df, "fs_ndatablocks", n_per_jobid=10)
-    #windows()
-    #plot_one_yvar(df, "fs_nmetablocks", n_per_jobid=10)
+   
 
+    #df = list.ss[['_extstats']]
+    #ss_plot_meta_file(df, n_per_jobid=100)
+
+    #########
+    df = list.ss[['_extstatssum']]
+    plot_one_yvar(df, "fs_ndatablocks", n_per_jobid=5)
+    windows()
+    plot_one_yvar(df, "fs_nmetablocks", n_per_jobid=5)
+    df$Number.Of.Medadata.Block.Per.Data.Block = with(df, fs_nmetablocks/fs_ndatablocks)
+    windows()
+    plot_one_yvar(df, "Number.Of.Medadata.Block.Per.Data.Block", n_per_jobid=10)
+
+    # df has the same jobid
+    get_ncur_files <- function(df)
+    {
+        df = arrange(df, monitor_time)
+        n = nrow(df)
+        df$season = (1:n)-1
+        tmp = with(df[1,], np*ndir_per_pid*nfile_per_dir*nwrites_per_file)
+        df$n_cur_files = tmp * 3
+        df$n_cur_files[1] = tmp
+        df$n_cur_files[2] = tmp*2
+        return (df)
+    }
+    df = ddply(df, .(jobid), get_ncur_files)
+    df$Number.Of.Metadata.Block.Per.Write = 
+        with(df, fs_nmetablocks/n_cur_files)
+    windows()
+    plot_one_yvar(df, "Number.Of.Metadata.Block.Per.Write", n_per_jobid=10)
+
+    #########
     #df = list.ss[['_freefrag_sum']]
     #plot_freefrag_sum(df, n_per_jobid=100)
 
-    df = list.ss[['_freefrag_hist']]
-    ss_plot_exthist_bar(df, dotext=F)
+    #########
+    #df = list.ss[['_freefrag_hist']]
+    ##ss_plot_exthist_bar(df, dotext=F)
+    #ss_plot_exthist_bar(df, dotext=F, col2set="Free_extents")
+
 }
 
+# for results with extlist
+plot_physical_blocks <- function(df, nseasons)
+{
+    tms = levels(df$monitor_time)
+    pickedtms = head(tms, n=nseasons)
+    
+    df = subset(df, monitor_time %in% pickedtms)# & wsize==1 & wstride==4096 )
+    
+    df$rowid = row.names(df)
+    rowsize = 1000
+    df = ddply(df, .(rowid), ddply_trans_wide, rowsize=rowsize)
+    df$y = df$y *rowsize
+
+    # clearer annotation
+    df$wstride = factor(df$wstride)
+    df$wsize = factor(df$wsize)
+    levels(df$wstride) = paste("Stride:", levels(df$wstride))
+    levels(df$wsize) = paste("Size:", levels(df$wsize))
+    p <- ggplot(df, aes()) +
+        geom_segment(aes(x=x.start, xend=x.end+1,
+                         y=y, yend=y,
+                         color=filepath), size=1)+
+        facet_grid(wstride~wsize) +
+        theme( axis.text.x = element_blank() ) +
+        xlab("") +
+        ylab("block number") +
+        ylim(c(0, 65535))
+    print (p)
+}
+
+# the input has to be only one row
+ddply_trans_wide <- function(df, rowsize=10000)
+{
+    start.row.y = floor(df$Physical_start/rowsize)
+    start.row.x = df$Physical_start%%rowsize
+    
+    end.row.y = floor(df$Physical_end/rowsize)
+    end.row.x = df$Physical_end%%rowsize
+    
+    if (end.row.y == start.row.y) {
+        df$x.start = start.row.x
+        df$x.end = end.row.x
+
+        df$y = start.row.y
+        return (df)
+    } else {
+        tp = head(df, n=1) 
+        tp$x.start = 0
+        tp$x.end = 0
+        tp$y = 0
+        
+        # for start row
+        r.start = tp
+        r.start$x.start = start.row.x
+        r.start$x.end = rowsize 
+        r.start$y = start.row.y
+        
+        # for end row
+        r.end = tp
+        r.end$x.start = 0
+        r.end$x.end = end.row.x
+        r.end$y = end.row.y
+
+        df.ret = rbind(r.start, r.end)
+        if ( end.row.y > start.row.y + 1 ) {
+            # there's line(s) in the middle
+            for ( i in (start.row.y+1):(end.row.y-1)){
+                r.mid = tp
+                r.mid$x.start = 0
+                r.mid$x.end = rowsize
+                r.mid$y = i
+
+                df.ret = rbind(df.ret, r.mid)
+            }
+        }
+        return(df.ret)
+    }
+
+}
+
+
+ss_main2 <- function() 
+{
+    #list.ss2 <<- files2df("C:/Users/Jun/Dropbox/0-Research/0-metadata/datahub/h0")
+   
+    df = list.ss2[['_extlist']]
+    plot_physical_blocks(df, 1)
+
+
+    #df = list.ss[['_extstats']]
+    #ss_plot_meta_file(df, n_per_jobid=100)
+
+    #########
+    #df = list.ss[['_extstatssum']]
+    #plot_one_yvar(df, "fs_ndatablocks", n_per_jobid=5)
+    #windows()
+    #plot_one_yvar(df, "fs_nmetablocks", n_per_jobid=5)
+    #df$Number.Of.Medadata.Block.Per.Data.Block = with(df, fs_nmetablocks/fs_ndatablocks)
+    #windows()
+    #plot_one_yvar(df, "Number.Of.Medadata.Block.Per.Data.Block", n_per_jobid=10)
+
+    ## df has the same jobid
+    #get_ncur_files <- function(df)
+    #{
+        #df = arrange(df, monitor_time)
+        #n = nrow(df)
+        #df$season = (1:n)-1
+        #tmp = with(df[1,], np*ndir_per_pid*nfile_per_dir*nwrites_per_file)
+        #df$n_cur_files = tmp * 3
+        #df$n_cur_files[1] = tmp
+        #df$n_cur_files[2] = tmp*2
+        #return (df)
+    #}
+    #df = ddply(df, .(jobid), get_ncur_files)
+    #df$Number.Of.Metadata.Block.Per.Write = 
+        #with(df, fs_nmetablocks/n_cur_files)
+    #windows()
+    #plot_one_yvar(df, "Number.Of.Metadata.Block.Per.Write", n_per_jobid=10)
+
+    #########
+    #df = list.ss[['_freefrag_sum']]
+    #plot_freefrag_sum(df, n_per_jobid=100)
+
+    #########
+    #df = list.ss[['_freefrag_hist']]
+    ##ss_plot_exthist_bar(df, dotext=F)
+    #ss_plot_exthist_bar(df, dotext=F, col2set="Free_extents")
+
+}
+
+
+
+
 # input should be _free_fraghist
-ss_plot_exthist_bar <- function(df, monitor_by=1, ptitle="PlotTitle", manjust=5, dotext=T)
+ss_plot_exthist_bar <- function(df, monitor_by=1, ptitle="PlotTitle", manjust=5, dotext=T, col2set="Percent")
 {
     # in case of too many monitors, pick some of them
     monitors = levels(df$monitor_time) 
@@ -345,24 +530,22 @@ ss_plot_exthist_bar <- function(df, monitor_by=1, ptitle="PlotTitle", manjust=5,
     df$Percentpos = df$Percent+manjust
 
     df = ddply(df, .(jobid, monitor_time), ss_pickandset, all_exts=unique(df$Ext_range),
-                                             col2set="Percent")
+                                             col2set=col2set)
     
     df$wstride = factor(df$wstride)
     df$wsize = factor(df$wsize)
-
     # clearer annotation
     levels(df$wstride) = paste("Stride:", levels(df$wstride))
     levels(df$wsize) = paste("Size:", levels(df$wsize))
-    print (summary(df$wsize))
-    p <- ggplot(df, aes(x=Ext_range, y=Percent, 
-                        color=monitor_time,
-                        fill=monitor_time,
-                        group=monitor_time))+
+
+    p <- ggplot(df, aes_string(x="Ext_range", y=col2set, 
+                        color="monitor_time",
+                        fill="monitor_time",
+                        group="monitor_time"))+
         #geom_line(alpha=3/3) +
         #geom_jitter(size=5, alpha=3/3, position = position_jitter(height = 0, width = 0.3)) +
         #geom_point(position='dodge')+
         geom_bar(position='dodge',stat='identity', drop=F)+
-        ylab("Percent (fregment size/FS capacity)")+
         xlab("Extent Ranges")+
         ggtitle(ptitle) +
         facet_grid(wsize~wstride)+
@@ -404,5 +587,53 @@ ss_pickandset <- function(df, all_exts, col2set="Free_extents") {
     ret = rbind(df, df.missed)
     return (ret)
 }
+
+
+
+#####################################################
+#####################################################
+#    search stride and nwrites
+
+
+sw_main <- function() 
+{
+    #list.sw <<- files2df("C:/Users/Jun/Dropbox/0-Research/0-metadata/datahub/h1.stride.nwrite")
+   
+    df = list.sw[['_extstatssum']]
+    sw_plot_one_yvar(df, "fs_ndatablocks", n_per_jobid=20)
+    windows()
+    sw_plot_one_yvar(df, "fs_nmetablocks", n_per_jobid=20)
+    df$Number.Of.Medadata.Block.Per.Data.Block = with(df, fs_nmetablocks/fs_ndatablocks)
+    windows()
+    sw_plot_one_yvar(df, "Number.Of.Medadata.Block.Per.Data.Block", n_per_jobid=20)
+
+}
+
+sw_plot_one_yvar <- function(df, seecol, n_per_jobid=10, dotext=F)
+{
+    df = ddply(df, .(jobid), head, n=n_per_jobid)
+    #df$wstride = factor(df$wstride)
+    df$nwrites_per_file = factor(df$nwrites_per_file)
+    levels(df$nwrites_per_file) = paste("NWritesPerFile:", levels(df$nwrites_per_file))
+    print(head(df))
+    df$ypos = df[,c(seecol)]*0.8
+    print("here i am")
+    p <- ggplot(df, aes_string(x="factor(wstride)", y=seecol, fill="monitor_time")) +
+        geom_bar(stat='identity', position='dodge', drop=F) +
+        scale_x_discrete(drop=F) +
+        facet_wrap(~nwrites_per_file) +
+        xlab("Write stride")
+    if ( dotext ) {
+        p = p + 
+            geom_text(aes_string(label=seecol, y="ypos"), position=position_dodge(width=1), angle=90)
+    }
+    print(p)
+}
+
+
+
+
+
+
 
 
