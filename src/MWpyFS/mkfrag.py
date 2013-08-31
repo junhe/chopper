6f0c4c874b4bbd7fc3b1a5bee6624a930bae6f8c
Jun He <junnhe@gmail.com>
2013-08-31 09:26:08 -0700

    initial test seems good for fragplan

--- src/MWpyFS/mkfrag.py
+++ src/MWpyFS/mkfrag.py
@@ -24,12 +24,60 @@ def frags(alpha, beta, count, sum_lim):
         fragsz.append( sum_lim*x/sm )
     plothist(fragsz)
         
-def fragplan():
+def fragplan(group_sizes, frag_sizes):
     """
+    input: a list of sizes, each representing a group
+           a list of sizes, each representing size of a fragment
+    output: multiple sets of sizes, each of which is for one group
     """
-    pass
+    ngrps = len(group_sizes)
+    nfrags = len(frag_sizes)
+    # a table, row i has frag sizes for group i
+    grp_frags = [[] for i in range(ngrps)  ] 
+    # a list, i has free space left for group i
+    free_grp_spaces = list(group_sizes) # shallow copy
+    
+    sum_grp_sizes = 0
+    for x in group_sizes:
+        sum_grp_sizes += x
 
-frags(0.87, 0.3, 1000, 100)
+    sum_frag_impl_sizes = 0
+    for x in frag_sizes:
+        sum_frag_impl_sizes += x
+    # to separate one segment to two fragments, you need
+    # at least one block in the middle of the two fragments
+    # To separate one segment to n fragments, you need
+    # at least n-1 barriers. However, by having k groups,
+    # you already have k-1 barriers. You all you need is
+    # (n-1) - (k-1) barriers.
+    sum_frag_impl_sizes += (nfrags - 1) - (ngrps - 1)
+
+    if sum_frag_impl_sizes > sum_grp_sizes:
+        print "fragment size exceeds group sizes",
+        exit(1)
+
+    # the allocation algorithm is greedy.
+    # pick the largest fragment and put it in the group
+    # with largest free space
+    frag_sizes = sorted(frag_sizes, reverse=True)
+    for frg in frag_sizes:
+        space = sorted(free_grp_spaces, reverse=True)[0]
+        g_idx = free_grp_spaces.index(space)
+
+        if frg <= space:
+            # put frg in group[g_idx]
+            grp_frags[g_idx].append(frg)
+            free_grp_spaces[g_idx] -= (frg+1) # +1 for the 
+                                               #barrier after this frag
+        else:
+            print "not good, this should not happend"
+            exit(1)
+
+    print grp_frags
+
+fragplan([10, 10], [2,3,9,2])
+
+#frags(100, 8, 1000, 100)
 
 #a = stats.lognorm(scale=0.1)
 #a = np.random.beta(0.75, 0.25, 10000)
