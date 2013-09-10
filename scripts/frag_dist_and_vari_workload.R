708401d934a285f09bb0f12eaa42e348efc5e77b
Jun He <junnhe@gmail.com>
2013-09-10 11:25:35 -0500

    ploting free space hist now can use locally stored Rdata

--- scripts/frag_dist_and_vari_workload.R
+++ scripts/frag_dist_and_vari_workload.R
@@ -62,60 +62,74 @@ pick_by_stride <- function(keys, stride)
     return (keys[selects])
 }
 
-fdist_free_space_hist <- function(df)
+# You speicify either df or rfile. 
+# rfile has to have a data frame named df
+fdist_free_space_hist <- function(df=NULL, rfile="")
 {
-    # pick monitors
-    pickedMons = pick_by_stride(unique(df$monitor_time), stride=1)
-    df = subset(df, monitor_time %in% pickedMons)
+    if ( is.null(df) ) {
+        load(file=rfile)#, globalenv())
+    } else {
+        # pick monitors
+        pickedMons = pick_by_stride(unique(df$monitor_time), stride=5)
+        df = subset(df, monitor_time %in% pickedMons)
 
+        # build factor
+        df$Free_Space_Dist_ID = paste("alpha:", df$alpha, ",", "beta:", df$beta, sep="")
+        df$Free_Space.Dist_ID = factor(df$Free_Space_Dist_ID)
+        df$Workload_ID = paste("np", df$np, ",",
+                               "nd", df$ndir_per_pid, ",",
+                               "nf", df$nfile_per_dir, ",",
+                               "nw", df$nwrites_per_file, "\n",
+                               "ws", df$wsize, ",",
+                               "wst", df$wstride, ",",
+                               "so", df$startoff,
+                               sep="")
+        df$Workload_ID = factor(df$Workload_ID)
 
-    df$Free_Space_Dist_ID = paste("alpha:", df$alpha, ",", "beta:", df$beta, sep="")
-    df$Free_Space.Dist_ID = factor(df$Free_Space_Dist_ID)
-    df$Workload_ID = paste("np", df$np, ",",
-                           "nd", df$ndir_per_pid, ",",
-                           "nf", df$nfile_per_dir, ",",
-                           "nw", df$nwrites_per_file, "\n",
-                           "ws", df$wsize, ",",
-                           "wst", df$wstride, ",",
-                           "so", df$startoff,
-                           sep="")
-    df$Workload_ID = factor(df$Workload_ID)
+        # pick free spaces
+        nFree_Space_Dist_ID = 6 
+        distids = unique(df$Free_Space_Dist_ID)
+        print(distids)
+        picked_distids = head(distids, n=nFree_Space_Dist_ID)
+        df = subset(df, Free_Space_Dist_ID %in% picked_distids)
 
-    # pick free spaces
-    nFree_Space_Dist_ID = 6 
-    distids = unique(df$Free_Space_Dist_ID)
-    print(distids)
-    picked_distids = head(distids, n=nFree_Space_Dist_ID)
-    df = subset(df, Free_Space_Dist_ID %in% picked_distids)
+        # fragment data calculation
+        df$Fragment_Block_Count = df$end - df$start + 1
+        df$Fragment_Log2Size = log(df$Fragment_Block_Count*4096, 2)
+        
+        #print(summary(df$Workload_ID))
+        #save(df, file="h7check01.freespacehist.2.Rdata")
+    }
 
-    # pick workloads
-    nWorkload_ID = 20 
-    wlids = unique(df$Workload_ID)
-    print(wlids)
-    picked_wlids = head(wlids, n=nWorkload_ID)
-    df = subset(df, Workload_ID %in% picked_wlids)
+    lvls = unique( as.character(df$Workload_ID) )
+    df$Workload_ID = factor(df$Workload_ID, levels=lvls)
 
+    mlvl = length(levels(df$Workload_ID))
+    ranges = list( 1:11, 12:22, 23:33) 
 
-    df$Fragment_Block_Count = df$end - df$start + 1
-    df$Fragment_Log2Size = log(df$Fragment_Block_Count*4096, 2)
-    
-    print(head(df))
-    
-    p = ggplot(df, aes(
-                       x=Fragment_Log2Size,
-                       #x=Fragment_Block_Count, 
-                       color=monitor_time
-                       )) +
-        geom_freqpoly(binwidth=1)+
-        #geom_histogram(aes(fill=monitor_time), position='dodge') +
-        #geom_density()+
-        facet_grid(Free_Space_Dist_ID~Workload_ID)+
-        scale_x_continuous(breaks=seq(12, 28, by=2),
-                           labels=(2^seq(12, 28, by=2)/1024))+
-        xlab("Fragment Size(KB)")+
-        theme(axis.text.x=element_text(angle=45,hjust=1))+
-        scale_y_log10()
-    print(p)
+    for (myrng in ranges) {
+        print(myrng)
+        pickedWkload = levels(df$Workload_ID)[intersect(myrng, 1:mlvl)]
+        print(pickedWkload)
+        dfp = subset(df, Workload_ID %in% pickedWkload)
+
+        p = ggplot(dfp, aes(
+                           x=Fragment_Log2Size,
+                           #x=Fragment_Block_Count, 
+                           color=monitor_time
+                           )) +
+            geom_freqpoly(binwidth=1)+
+            #geom_histogram(aes(fill=monitor_time), position='dodge') +
+            #geom_density()+
+            facet_grid(Free_Space_Dist_ID~Workload_ID)+
+            scale_x_continuous(breaks=seq(12, 28, by=2),
+                               labels=(2^seq(12, 28, by=2)/1024))+
+            xlab("Fragment Size(KB)")+
+            theme(axis.text.x=element_text(angle=45,hjust=1))+
+            scale_y_log10(limits=c(0.001, 1e6))
+        windows()
+        print(p)
+    }
 }
 
 fdist_meta_data_blocks <- function(df)
@@ -135,26 +149,36 @@ fdist_meta_data_blocks <- function(df)
                            "wst", df$wstride, ",",
                            "so", df$startoff,
                            sep="")
-    df$Workload_ID = factor(df$Workload_ID)
+    lvls = unique( as.character(df$Workload_ID) )
+    df$Workload_ID = factor(df$Workload_ID, levels=lvls)
 
+    mlvl = length(levels(df$Workload_ID))
+    ranges = list( 1:11, 12:22, 23:33) 
 
-    p = ggplot(df, aes(
-                       x=monitor_time,
-                       y=fs_nmetablocks,
-                       #y=fs_ndatablocks,
-                       fill=monitor_time
-                       )) +
-        #geom_histogram(position='dodge') +
-        geom_bar(stat="identity", position='dodge')+
-        geom_text(aes(label=fs_nmetablocks, y=fs_nmetablocks), 
-                    position=position_dodge(width=1),
-                  size=4, angle=90)+
-        facet_grid(Free_Space_Dist_ID~Workload_ID)+
-        theme(axis.text.x=element_text(angle=45,hjust=1))+
-        #theme(strip.text.x=element_text(size=5))+
-        xlab("Time")
-    print("after ggplot")
-    print(p)
+    for (myrng in ranges) {
+        print(myrng)
+        pickedWkload = levels(df$Workload_ID)[intersect(myrng, 1:mlvl)]
+        dfp = subset(df, Workload_ID %in% pickedWkload)
+
+        p = ggplot(dfp, aes(
+                           x=monitor_time,
+                           y=fs_nmetablocks,
+                           #y=fs_ndatablocks,
+                           fill=monitor_time
+                           )) +
+            #geom_histogram(position='dodge') +
+            geom_bar(stat="identity", position='dodge')+
+            geom_text(aes(label=fs_nmetablocks, y=fs_nmetablocks), 
+                        position=position_dodge(width=1),
+                      size=4, angle=90)+
+            facet_grid(Free_Space_Dist_ID~Workload_ID)+
+            theme(axis.text.x=element_text(angle=45,hjust=1))+
+            theme(strip.text.x=element_text(size=8))+
+            xlab("Time")+ylim(c(0,4500))
+        print("after ggplot")
+        windows()
+        print(p)
+    }
 }
 
 
@@ -209,7 +233,22 @@ h6ck00_main <- function()
     
 
     #fdist_meta_data_blocks(list.h7check00[['_extstatssum']])
-    fdist_free_space_hist(list.h7check00[['_freeblocks']])
+    #fdist_free_space_hist(list.h7check00[['_freeblocks']])
 
+
+    #############################
+    #############################
+    #############################
+    #############################
+    #############################
+    list.h7check01 <<- files2df("C:/Users/Jun/Documents/Workdir/h7.check01")
+    save(list.h7check01, file="list.h7check01.all.Rdata")
+    print(str(list.h7check01))
+    #load(file="list.h7check01.Rdata", globalenv())
+
+
+    #fdist_meta_data_blocks(list.h7check01[['_extstatssum']])
+    #fdist_free_space_hist(list.h7check01[['_freeblocks']])
+    #fdist_free_space_hist(file="h7check01.freespacehist.2.Rdata")
 }
 
