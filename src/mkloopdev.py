761985c681ed0feba77a26fed01a8396757ddfcf
Jun He <jhe@cs.wisc.edu>
2013-11-09 11:06:18 -0500

    now it is more user-friendly! it asks.

--- src/mkloopdev.py
+++ src/mkloopdev.py
@@ -8,9 +8,21 @@ MWpyFS.FormatFS.makeLoopDevice(
         devname="/dev/loop0",
         tmpfs_mountpoint="/mnt/mytmpfs",
         sizeMB=4096)
-exit()
+
+print "Loop device has been made :)"
+
 if not os.path.exists("/mnt/scratch"):
     print "/mnt/scratch does not exist. Creating..."
     os.mkdir("/mnt/scratch")
-MWpyFS.FormatFS.remakeExt4("/dev/loop0", "/mnt/scratch/", "jhe", "plfs", 
-                blockscount=1024*1024, blocksize=4096)
+
+fs_choice = raw_input(
+        "What file system do you want to mount on the device?([1]xfs|[2]ext4):")
+
+if fs_choice.lower() == 'ext4' or fs_choice == '2':
+    MWpyFS.FormatFS.remakeExt4("/dev/loop0", "/mnt/scratch/", "jhe", "plfs", 
+                    blockscount=1024*1024, blocksize=4096)
+
+if fs_choice.lower() == 'xfs' or fs_choice == '1':
+    MWpyFS.FormatFS.remakeXFS('/dev/loop0', '/mnt/scratch', 'jhe', 'plfs',
+                                blocksize=4096)
+
