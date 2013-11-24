90a4dd492d455e31d67675b684234da6b215073c
Jun He <junnhe@gmail.com>
2013-11-24 08:11:53 -0800

    moved pattern iterator related functions to pattern_iter.py

--- src/pyWorkload/pattern_iter.py
+++ src/pyWorkload/pattern_iter.py
@@ -1,6 +1,42 @@
 import itertools
 import producer
 
+def Filter( opstr, keep ):
+    # input is like (FSYNC, True)
+    if keep:
+        return opstr 
+    else:
+        return ""
+
+def wrappers_to_symbols( wrappers ):
+    num_of_chunks = len(wrappers) / 4
+    #strs = ['OPEN', 'FSYNC', 'CLOSE', 'SYNC'] * num_of_chunks
+    symbols = ['(', 'C', 'F', ')', 'S'] * num_of_chunks
+
+    choices = [ (wrappers[i], True, wrappers[i+1], wrappers[i+2], wrappers[i+3]) \
+                    for i in range(0, num_of_chunks*4, 4) ]
+    choices = list(itertools.chain.from_iterable(choices))
+    seq = map(Filter, symbols, choices)
+    seq = ''.join(seq)
+
+    return seq
+
+
+def IsLegal( wrappers ):
+    "conver to string and use regex"
+    seq = wrappers_to_symbols( wrappers )
+   
+    # use regex to check seq
+    #
+    # GOOD ONE backup: mo = re.match(r'^(\((C+F?)+\)S?)+$', seq, re.M)
+    #mo = re.match(r'^(\((C+F)+\))S$', seq, re.M) # always Fsync, only one open-close, must sync
+    mo = re.match(r'^(\((C+F?)+\)S)+$', seq, re.M)
+    if mo:
+        print "good match!", seq
+        return True
+    else:
+        #print 'BAD match!', seq
+        return False
 
 def pattern_iter(nfiles, filesize, chunksize):
     """
@@ -32,12 +68,58 @@ def pattern_iter(nfiles, filesize, chunksize):
     for chks in chunk_iter:
         wrapper_iter = itertools.product([False, True], repeat=num_of_chunks*4)
         for wraps in wrapper_iter:
-            if not producer.IsLegal(wraps):
+            if not IsLegal(wraps):
                 # skip bad ones
                 continue
             #print chks
             #print wraps
             yield {'chunks':chks, 'wrappers':wraps}
+
+def GenWorkloadFromChunks( chunks,
+                           wrappers,
+                           rootdir,
+                           tofile
+                           ):
+        
+    num_of_chunks = len(chunks)
+
+    # organize it as dictionary
+    chunks = [ dict( zip( ['offset', 'length'], c ) ) \
+                                                    for c in chunks ]
+    # group wrapers to 3 element groups
+    wrappers = [wrappers[i:i+4] for i in range(0, num_of_chunks*4, 4) ]
+    wrappers = [ dict( zip(['OPEN', 'FSYNC', 'CLOSE', 'SYNC'], w) ) \
+                                                    for w in wrappers]
+
+    prd = producer.Producer(
+            rootdir = rootdir,
+            tofile = tofile)
+    prd.addDirOp('mkdir', pid=0, dirid=0)
+
+    # ( (off,size), {wrapper} )
+    entries = zip(chunks, wrappers)
+    for entry in entries:
+        #print entry
+        if entry[1]['OPEN']:
+            prd.addUniOp('open', pid=0, dirid=0, fileid=0)
+        
+        # the chunk write
+        prd.addReadOrWrite('write', pid=0, dirid=0,
+               fileid=0, off=entry[0]['offset'], len=entry[0]['length'])
+
+        if entry[1]['FSYNC']: 
+            prd.addUniOp('fsync', pid=0, dirid=0, fileid=0)
+
+        if entry[1]['CLOSE']: 
+            prd.addUniOp('close', pid=0, dirid=0, fileid=0)
+
+        if entry[1]['SYNC']:
+            prd.addOSOp('sync', pid=0)
+
+    prd.display()
+    prd.saveWorkloadToFile()
+    return True
+
 
 #for x in pattern_iter(1, 6, 2):
     #print x
