bf85c5319131a6955328de37a923e1ba859a391f
Jun He <jhe@cs.wisc.edu>
2014-01-10 13:04:58 -0500

    now save result files to local disk first and move them to NFS later, for speed

--- src/quickrun.sh
+++ src/quickrun.sh
@@ -1,6 +1,7 @@
 testname=$1
 host=`hostname`
-resultdir=results.$host
+resultparent=/mnt/scratch-sda4/
+resultdir=/mnt/scratch-sda4/results.$host
 echo testname $testname 
 echo host: $host 
 echo result dir: $resultdir
@@ -10,12 +11,22 @@ then
     exit
 fi
 #sleep 1
-sudo rm -rf $resultdir \
-    && sudo python walkman-feedback.py ../conf/h0.conf \
-    && sleep 1 
+
+sudo rm -rf $resultdir
+sudo python walkman-feedback.py ../conf/h0.conf 
+sleep 1 
 sudo python ../scripts/result-parser-faster.py $resultdir $host  
-sudo rm ${testname}.tar.gz 
-sudo rm -rf ${testname}
-sudo mv $resultdir ${testname}
-tar zcvf ${testname}.tar.gz ${testname}/z*
+
+sudo rm -f $resultparent/${testname}.tar.gz 
+sudo rm -rf $resultparent/${testname}
+sudo mv $resultdir $resultparent/${testname}
+echo Compressing z files
+tar zcvf $resultparent/${testname}.tar.gz $resultparent/${testname}/z*
+
+echo copying $testname to NFS
+rm -rf ./${testname}-$host
+cp -r $resultparent/${testname} ./${testname}-$host
+echo copying $testname.tar.gz file to NFS
+rm -f ./${testname}-$host.tar.gz
+cp $resultparent/${testname}.tar.gz ./${testname}-$host.tar.gz
 
