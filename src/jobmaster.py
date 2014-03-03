d767759795946e5dcaf256e761349810b0ff5e9f
Jun He <jhe@cs.wisc.edu>
2014-03-03 10:54:39 -0500

    add jobmaster and worker. They can do echo of treatment at this moment

---
+++ src/jobmaster.py
@@ -0,0 +1,85 @@
+import multiprocessing
+import Queue
+import time
+from multiprocessing.managers import SyncManager
+import pyWorkload
+import pprint
+
+# This jobmaster uses workload iterator to find
+# jobs and put them to the job queue. The workers
+# grab jobs from the queue and put the reults to the 
+# result queue. When the master find the job queue
+# is empty, it will use the workload iterator to
+# generate more jobs.
+#
+# The results in the result queue will be saved
+# to a single file by the master. To prevent the
+# queue from taking too much memory, the info saved
+# should be de-queued.
+
+
+PORTNUM=8848
+AUTHKEY='11'
+
+def runserver():
+    # Start a shared manager server and access its queues
+    manager = make_server_manager(PORTNUM, AUTHKEY)
+    shared_job_q = manager.get_job_q()
+    shared_result_q = manager.get_result_q()
+
+
+    cnt = 10
+    for treatment in pyWorkload.exp_design.onefile_iter():
+        shared_job_q.put( treatment )
+        cnt -= 1
+        if cnt == 0:
+            break
+    #N =86 
+    #nums = range(N)
+
+    ## The numbers are split into chunks. Each chunk is pushed into the job
+    ## queue.
+    #chunksize = 43
+    #for i in range(0, len(nums), chunksize):
+        #shared_job_q.put(nums[i:i + chunksize])
+
+    # Wait until all results are ready in shared_result_q
+    numresults = 0
+    resultlist = []
+    while numresults < 2:
+        out = shared_result_q.get()
+        resultlist.append(out)
+        numresults += 1
+        
+    pprint.pprint( resultlist )
+
+    # Sleep a bit before shutting down the server - to give clients time to
+    # realize the job queue is empty and exit in an orderly way.
+    time.sleep(2)
+    manager.shutdown()
+
+def make_server_manager(port, authkey):
+    """ Create a manager for the server, listening on the given port.
+        Return a manager object with get_job_q and get_result_q methods.
+    """
+    job_q = Queue.Queue()
+    result_q = Queue.Queue()
+
+    # This is based on the examples in the official docs of multiprocessing.
+    # get_{job|result}_q return synchronized proxies for the actual Queue
+    # objects.
+    class JobQueueManager(SyncManager):
+        pass
+
+    JobQueueManager.register('get_job_q', callable=lambda: job_q)
+    JobQueueManager.register('get_result_q', callable=lambda: result_q)
+
+    manager = JobQueueManager(address=('', port), authkey=authkey)
+    manager.start()
+    print 'Server started at port %s' % port
+    return manager
+
+if __name__ == '__main__':
+    runserver()
+
+
---
+++ src/worker.py
@@ -0,0 +1,73 @@
+import multiprocessing
+import Queue
+import time
+from multiprocessing.managers import SyncManager
+
+PORTNUM=8848
+AUTHKEY='11'
+IP='127.0.0.1'
+
+
+def factorizer_worker(job_q, result_q):
+    """ A worker function to be launched in a separate process. Takes jobs from
+        job_q - each job a list of numbers to factorize. When the job is done,
+        the result (dict mapping number -> list of factors) is placed into
+        result_q. Runs until job_q is empty.
+    """
+    while True:
+        try:
+            job = job_q.get_nowait()
+            result_q.put(job)
+        except Queue.Empty:
+            return
+
+def mp_factorizer(shared_job_q, shared_result_q, nprocs):
+    """ Split the work with jobs in shared_job_q and results in
+        shared_result_q into several processes. Launch each process with
+        factorizer_worker as the worker function, and wait until all are
+        finished.
+    """
+    procs = []
+    for i in range(nprocs):
+        p = multiprocessing.Process(
+                target=factorizer_worker,
+                args=(shared_job_q, shared_result_q))
+        procs.append(p)
+        p.start()
+
+    for p in procs:
+        p.join()
+
+
+
+
+def runclient():
+    manager = make_client_manager(IP, PORTNUM, AUTHKEY)
+    job_q = manager.get_job_q()
+    result_q = manager.get_result_q()
+    mp_factorizer(job_q, result_q, 4)
+
+
+def make_client_manager(ip, port, authkey):
+    """ Create a manager for a client. This manager connects to a server on the
+        given address and exposes the get_job_q and get_result_q methods for
+        accessing the shared queues from the server.
+        Return a manager object.
+    """
+    class ServerQueueManager(SyncManager):
+        pass
+
+    ServerQueueManager.register('get_job_q')
+    ServerQueueManager.register('get_result_q')
+
+    manager = ServerQueueManager(address=(ip, port), authkey=authkey)
+    manager.connect()
+
+    print 'Client connected to %s:%s' % (ip, port)
+    return manager
+
+
+if __name__ == '__main__':
+    runclient()
+
+
