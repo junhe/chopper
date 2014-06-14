8ebbec69b2f640bfe1e9a34fc2185a67d4f47bfa
Jun He <jhe@cs.wisc.edu>
2014-06-14 15:55:27 -0400

    print more details of hostname in worker.

--- src/jobmaster.py
+++ src/jobmaster.py
@@ -27,8 +27,8 @@ AUTHKEY='11'
 
 def get_joblist():
     jobiter = pyWorkload.exp_design.\
-                #fourbyfour_iter('./designs/blhd-12-factors-4by4.txt')
                 fourbyfour_iter('./designs/blhd-12-factors-4by7.txt')
+                #fourbyfour_iter('./designs/blhd-12-factors-4by4.txt')
                 #fourbyfour_iter('./designs/blhd-11-factors-4by7.txt')
                 #fourbyfour_iter('./designs/design_blhd-4by4.tmp.txt')
                 #fourbyfour_iter('./design_blhd-4by4.txt')
