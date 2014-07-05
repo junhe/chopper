33053943ad54a3afb4570133fd02fa8c558129f1
Jun He <jhe@cs.wisc.edu>
2014-07-05 17:29:40 -0400

    change drivermoutopts setting

--- src/drivemountopts.py
+++ src/drivemountopts.py
@@ -5,15 +5,15 @@ import re
 import time
 
 #opts = "barrier=1,max_batch_time=0,noauto_da_alloc,data=writeback,noauto_da_alloc,commit=7200,norecovery,data=ordered,data=journal"
-#opts = "data=journal"
-opts = "data=writeback,data=ordered"
+#opts = "barrier=1,max_batch_time=0,noauto_da_alloc,noauto_da_alloc,commit=7200,norecovery,data=ordered,data=journal"
+opts = "nodelalloc"
 opts = opts.split(',')
 
 resultdir = 'trymount'
 
 # 1
 for optcomb in itertools.combinations(opts, 1):
-    for runid in range(3):
+    for runid in range(2):
         oparg = ','.join(optcomb)
         resultfile = re.sub(r'\W', '_', oparg)
         resultfile = resultfile + "." + time.strftime("%m-%d-%H-%M-%S", time.localtime())
