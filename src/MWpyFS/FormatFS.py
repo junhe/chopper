c3efb45f13608c85506611a6698e3941bd987c2b
Jun He <jhe@cs.wisc.edu>
2015-01-12 16:10:30 -0600

    minor change

--- src/MWpyFS/FormatFS.py
+++ src/MWpyFS/FormatFS.py
@@ -21,8 +21,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#!/usr/bin/python 
-
 import subprocess
 import os
 import re
