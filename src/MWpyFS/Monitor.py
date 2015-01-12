df53e97dc4560160f7c9f5664d74910afaea1dd0
Jun He <jhe@cs.wisc.edu>
2015-01-12 16:07:11 -0600

    add copyright in src/subdirs

--- src/MWpyFS/Monitor.py
+++ src/MWpyFS/Monitor.py
@@ -1,3 +1,26 @@
+# Chopper is a diagnostic tool that explores file systems for unexpected
+# behaviors. For more details, see paper Reducing File System Tail 
+# Latencies With Chopper (http://research.cs.wisc.edu/adsl/Publications/).
+#
+# Please send bug reports and questions to jhe@cs.wisc.edu.
+#
+# Written by Jun He at University of Wisconsin-Madison
+# Copyright (C) 2015  Jun He (jhe@cs.wisc.edu)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # The monitor is used to monitor the FS fragmentation status.
 # What I want to see is, generally, how's the metadata. This may include:
 #
--- src/pyWorkload/workload_builder.py
+++ src/pyWorkload/workload_builder.py
@@ -1,3 +1,26 @@
+# Chopper is a diagnostic tool that explores file systems for unexpected
+# behaviors. For more details, see paper Reducing File System Tail 
+# Latencies With Chopper (http://research.cs.wisc.edu/adsl/Publications/).
+#
+# Please send bug reports and questions to jhe@cs.wisc.edu.
+#
+# Written by Jun He at University of Wisconsin-Madison
+# Copyright (C) 2015  Jun He (jhe@cs.wisc.edu)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # This module uses # pat_data_struct and pattern_iter to create new types
 # of workloads.
 # We should keep pat_data_struct and pattern_iter more
